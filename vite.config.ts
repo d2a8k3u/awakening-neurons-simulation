974037a027fe9c2c9f461fc1e293a41fe02eb299
David Kulhánek <davidkulh182@gmail.com>
2025-11-10 01:10:58 +0700

    fix(config): correct base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'awakening-neurons-simulation',
   plugins: [react(), tailwindcss()],
+  base: '/awakening-neurons-simulation/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
